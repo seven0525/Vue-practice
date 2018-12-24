488c48fc00c0bedd2a9797add44a0ef17c57f8d0
渡辺大智 <c211587r@icu.ac.jp>
2018-12-25 03:25:20 +0900

    qiita-searcherの完成

--- qiita-searcher/src/App.vue
+++ qiita-searcher/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <p>
+      <input type="text" v-model="keyword"></input>
+    </p>
+    <p>
+     {{ message}}
+    </p>
+    <ul>
+      <li v-for="item in items">
+        <a v-bind:href="item.url" target="_blank">{{ item.title }}</a> likes:{{ item.likes_count }}
+      </li>
+    </ul>
+
+
   </div>
 </template>
 
@@ -15,17 +27,23 @@ export default {
     }
   },
   watch: {
+    keyword: function(newKeyword, oldKeyword){
+      this.message = "Waiting for you to stop typing ..."
+      this.debouncedGetAnswer()
+    }
 
   },
   created: function(){
-    this.keyword = "JavaScript"
-    this.getAnswer()
+    // this.keyword = "JavaScript"
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
 
   },
   methods: {
     getAnswer: function(){
       if(this.keyword == "") {
         this.items = null
+        this.message = ""
         return
       }
 
@@ -51,28 +69,28 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 
-h1, h2 {
-  font-weight: normal;
-}
+// h1, h2 {
+//   font-weight: normal;
+// }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+// li {
+//   // display: inline-block;
+//   margin: 0 10px;
+// }
 
 a {
   color: #42b983;
